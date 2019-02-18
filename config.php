8b848ef2ff9c7bd9f9ed91ea12faa4c8612ad200
Bent42 <yaannn16@gmail.com>
2019-02-18 13:33:15 -0400

    Iniciamos o projeto

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+spl_autoload_register(function($classname){
+
+	$filename = "classes".DIRECTORY_SEPARATOR.$classname.".php";
+	
+	if (file_exists($filename)) {
+		require_once($filename);
+	}
+});
+
+
+ ?>
